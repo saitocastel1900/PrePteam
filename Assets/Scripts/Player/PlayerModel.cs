797d625f1be93b97223f78356cd27755bb6f4d14
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-11-28 22:56:28 +0900

    feat：スクリプト（Playerのスクリプト）を追加しました

---
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -0,0 +1,32 @@
+using Commons.Enum;
+using UniRx;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerModel : MonoBehaviour
+    {
+        private BoolReactiveProperty _running;
+        public IReadOnlyReactiveProperty<bool> Running => _running;
+
+        //TODO:InGameEnumのリアクティブプロパティに変える
+        //TODO:Modelの_stateが使われていないので、こっちの_stateを使うようにする
+        public InGameEnum.State _state;
+
+        public void Initialized()
+        {
+            _state = InGameEnum.State.Stop;
+            _running = new BoolReactiveProperty(false);
+        }
+
+        public void UpdateState(InGameEnum.State state)
+        {
+            _state = state;
+        }
+
+        public void UpdateValue(bool isRun)
+        {
+            if(Running.Value!=isRun)_running.Value = isRun;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerPresenter.cs
@@ -0,0 +1,93 @@
+using Commons.Const;
+using Commons.Enum;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Player
+{
+    //TODO:コメントを書こう
+    public class PlayerPresenter : MonoBehaviour
+    {
+        [SerializeField] private PlayerModel _model;
+        [SerializeField] private PlayerView _view;
+        
+        private Vector3 _moveSpeed;
+        private Rigidbody _rigidbody;
+       
+        void Start()
+        {
+            Initialized();
+            Bind();
+            SetEvents();
+        }
+        
+        private void Update()
+        {
+            _model.UpdateState(_view.InputMove());
+        }
+
+        private void FixedUpdate()
+        {
+            OnStateChanged();
+            _rigidbody.velocity = _moveSpeed;
+            
+            Debug.Log(_moveSpeed);
+            //BUG:カメラで回転させるのでいらなくなる
+            if(_moveSpeed!=Vector3.zero)transform.forward = new Vector3(_moveSpeed.x,0,_moveSpeed.z);;
+        }
+
+        private void Initialized()
+        {
+            _view.Initialized();
+            _model.Initialized();
+            
+            _rigidbody = this.GetComponent<Rigidbody>();
+            _moveSpeed = Vector3.zero;
+        }
+
+        private void Bind()
+        {
+            this.gameObject.OnCollisionEnterAsObservable()
+                .Subscribe(target =>
+                {
+                    Debug.Log("衝突しました");
+                    var hit = target.gameObject.GetComponent<IPushable>();
+                    hit?.Push();
+                    Debug.Log("衝突してますよ");
+                }).AddTo(this);
+        }
+
+        private void SetEvents()
+        {
+            _model.Running.Subscribe(value=>_view.UpdateView(value)).AddTo(this);
+        }
+
+        private void OnStateChanged()
+        {
+            switch (_model._state)
+            {
+                case InGameEnum.State.Ahead:
+                    _moveSpeed = new Vector3(0, _rigidbody.velocity.y, InGameConst.MoveSpeed * 1);
+                    _model.UpdateValue(true);
+                    break;
+                case InGameEnum.State.Back:
+                    _moveSpeed = new Vector3(0, _rigidbody.velocity.y, InGameConst.MoveSpeed * -1);
+                    _view.UpdateView(true);
+                    break;
+                case InGameEnum.State.Left:
+                    _moveSpeed = new Vector3(InGameConst.MoveSpeed * -1, _rigidbody.velocity.y, 0);
+                    _view.UpdateView(true);
+                    break;
+                case InGameEnum.State.Right:
+                    _moveSpeed = new Vector3(InGameConst.MoveSpeed * 1, _rigidbody.velocity.y, 0);
+                    _view.UpdateView(true);
+                    break;
+                case InGameEnum.State.Stop:
+                    _moveSpeed = new Vector3(_rigidbody.velocity.x, _rigidbody.velocity.y, _rigidbody.velocity.z);
+                    _view.UpdateView(false);
+                    break;
+            }
+        }
+    }
+}
