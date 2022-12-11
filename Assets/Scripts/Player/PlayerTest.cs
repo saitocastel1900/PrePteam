01f85041662fde4d3b41c7e3c4db3d497f014bd4
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-12-11 21:25:57 +0900

    feat：プレイヤーの動きに関するスクリプトを追加

--- Assets/Scripts/Player/PlayerTest.cs
+++ Assets/Scripts/Player/PlayerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerTest : MonoBehaviour
@@ -11,6 +12,12 @@ public class PlayerTest : MonoBehaviour
 
     private void Update()
     {
+        var horizontal = UnityEngine.Input.GetAxis("Horizontal");
+        var vertical = UnityEngine.Input.GetAxis("Vertical");
+        var velocity = new Vector3(horizontal,0, vertical).normalized;
+        var speed = UnityEngine.Input.GetKey(KeyCode.LeftShift) ? 2 : 1;
         
+        Debug.Log(velocity.magnitude);
+        _animator.SetFloat("Speed",velocity.magnitude*speed,0.1f,Time.deltaTime);
     }
 }
